4c7dd3cb20d93cdcf195af113f87afc33eeb352a
jasha.chec <jasha@chec.de>
2024-03-28 17:12:46 +0100

    chore(): init dotfiles

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,38 @@
+return {
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
+			}
+		end,
+	},
+}
+-- return {
+-- 	{
+-- 		"catppuccin/nvim",
+-- 		name = "catppuccin",
+-- 		lazy = true,
+-- 		priority = 1000,
+-- 	},
+-- }
+-- return {
+-- 	{
+-- 		"projekt0n/github-nvim-theme",
+-- 		layz = true,
+-- 		priority = 1000,
+-- 		config = function()
+-- 			vim.cmd("colorscheme github_dark_colorblind")
+-- 		end,
+-- 	},
+-- }
+-- return {
+-- 	{
+-- 		"rebelot/kanagawa.nvim",
+-- 		lazy = true,
+-- 		priority = 1000,
+-- 		name = "kinagawa",
+-- 	},
+-- }
